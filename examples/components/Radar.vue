010d16ff859065e780a88e4d7888e098e30b3636
Mudin <muhridin@next-aeon.com>
2018-03-30 15:24:48 +0900

    Radar handler

--- examples/components/Radar.vue
+++ examples/components/Radar.vue
@@ -6,17 +6,28 @@
 */
 
 <template>
-<div>
-  <v-wedge :config='config' ref="wedge"></v-wedge>
-
-  <!-- not working in the moment, it is handler for diretion -->
-  <v-shape :config='config.handler'></v-shape>   
-</div>
+<v-group>
+  <v-wedge :config='config' ref="wedge"
+    @dragstart='onDragStart'
+    @dragmove='onDragMove'
+    @dragend='onDragEnd'>
+  </v-wedge>
+  <v-circle 
+    :config='handler' 
+    ref='handler'
+    @dragstart='onHandlerDragStart'
+    @dragmove='onHandlerDragMove'
+    @dragend='onHandlerDragEnd'>
+  </v-circle>   
+</v-group>
   
 </template>
 
 <script>
 import {Werge} from 'konva'
+
+let tween;
+
 export default {
   name: 'Radar',
   extends: Werge,
@@ -26,34 +37,83 @@ export default {
       default: {
         direction: 0
       }
+    },
+    handler: {
+      type: Object,
+      default: {
+        draggable: true
+      }
     }
   },
   watch: {
-    'config.angle': function (newVal, oldVal) {
-      this.handleAngleChange(true);
+    'config.angle': function() {
+      this.handleAngleChange()
     },
-    'config.direction': function (newVal, oldVal) {
-      this.handleDirectionChange(false);
+    'config.direction': function() {
+      this.handleAngleChange()
+    },
+    'config.x': function() {
+      this.resetHandler()
+    },
+    'config.y': function() {
+      this.resetHandler()
     }
   },
   methods: {
     handleAngleChange(angle) {
       this.config.rotation = this.config.direction - this.config.angle / 2.0
+      this.resetHandler()
     },
-    handleDirectionChange(angle) {
-      // todo need better solution
-      // const shape = this.$parent.getStage().children[0]
+    resetHandler() {
+      var vm = this;
 
-      // got better solution now
-      const shape = this.$refs.wedge.getStage()
+      vm.config.direction = Number(vm.config.direction)
+      var r = vm.config.radius * 0.7; // distance from origin of radar is 70% of radar's radiuss
 
-      var tween = new Konva.Tween({
-        node: shape,
-        duration: 0.3,
-        rotation: this.config.direction - this.config.angle / 2.0
-      });
-      tween.play()
+      vm.$refs.handler.getStage().setX(r * Math.cos(vm.config.direction * DEG2RAD) + vm.config.x);
+      vm.$refs.handler.getStage().setY(r * Math.sin(vm.config.direction * DEG2RAD) + vm.config.y);
+    },
+    onDragStart() {
+      console.log('onDragStart')
+    },
+    onDragEnd() {
+      console.log('onDragEnd')
+    },
+    onDragMove(target, e) {
+      console.log('onDragMove', e, target)
+      this.config.x = e.target._lastPos.x;
+      this.config.y = e.target._lastPos.y;
+      this.resetHandler()
+    },
+    onHandlerDragStart(target, e) {
+      console.log('onHandlerDragStart');
+      target.config.opacity = 0.5;
+      this.resetHandler();
+    },
+    onHandlerDragEnd(target, e) {
+      console.log('onHandlerDragEnd')
+      target.config.opacity = 1
+      this.resetHandler()
+    },
+    onHandlerDragMove(target, e) {
+      console.log('onHandlerDragMove', e, target)
+
+      // radar's position
+      var x0 = this.config.x;
+      var y0 = this.config.y;
+
+      // handler's position
+      var x = e.target._lastPos.x;
+      var y = e.target._lastPos.y;
+
+      this.config.direction = RAD2DEG * Math.atan2(y - y0, x - x0);
+
+      this.resetHandler();
     }
   }
 };
+
+const RAD2DEG = 180.0 / Math.PI;
+
+const DEG2RAD = Math.PI / 180.0;
 </script>
--- examples/components/RadarDemo.vue
+++ examples/components/RadarDemo.vue
@@ -11,7 +11,8 @@
     <v-stage ref="stage" :config="configKonva">
       <v-layer ref="layer">
         <v-radar
-          :config="wedge">
+          :config="wedge"
+          :handler="handler">
         </v-radar>
       </v-layer>
       <v-layer ref="dragLayer"></v-layer>
@@ -33,7 +34,8 @@ export default {
         width: width,
         height: height
       },
-      wedge: {}
+      wedge: {},
+      handler: {}
     };
   },
   components: {
@@ -45,28 +47,29 @@ export default {
     vm.wedge = {
       x: vm.configKonva.width / 2,
       y: vm.configKonva.height / 2,
-      radius: 100,
+      radius: 160,
       angle: 60,
       direction: 90,
       draggable: true,  // todo: after dragging, changing direction and angle makes radar to go back initial position
       fillRadialGradientStartPoint: 0,
       fillRadialGradientStartRadius: 0,
       fillRadialGradientEndPoint: 0,
-      fillRadialGradientEndRadius: 100,
+      fillRadialGradientEndRadius: 160,
       fillRadialGradientColorStops: [0, 'red', 0.3, 'blue', 0.5, 'rgba(0,0,255,0.6)', 1, 'rgba(255,255,255,0)'],
       opacity: 0.8,
       stroke: 'transparent',
       strokeWidth: 1,
       scaleX: 1,
       scaleY: 1,
-      startScale: 1,
-      handler: vm.wedgeHangler   // todo: draw handler for radar to rotate (to change direction)
+      startScale: 1
     }
-    vm.wedgeHangler = {
+
+    vm.handler = { // todo: draw handler for radar to rotate (to change direction)
       radius: 10,
-      angle: 60,
-      opacity: 0.8,
-      strokeWidth: 1
+      strokeWidth: 2,
+      stroke: 'rgba(0,0,255,0.5)',
+      fill: 'rgba(255,255,255,1)',
+      draggable: true // this should be always true by default
     }
   }
 };
